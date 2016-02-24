eee8a7e105fc796791aea715888aa2a7a8461d3a
jfpalomeque <jfpalomeque.gonzalez@gmail.com>
2016-02-24 15:30:18 +0100

    24/02/16

--- R/select.Anatomic_Part.R
+++ R/select.Anatomic_Part.R
@@ -2,4 +2,4 @@ select.Anatomic_Part <-
   function(){ #Ask user for Anatomic Part
     print(paste("Name image is ",files_names[i]))
     print("Seleccione Anatomic Part = ")
-    switch(menu(c("Humero", "Femur", "Radious", "Tibia", "Ulna", "Metacarp", "Metatarse", "Pelvis", "Scapule", "Rib", "Vertebre", "Mandible", "Falange", "Unknow")), "Humero", "Femur", "Radious", "Tibia", "Ulna", "Metacarp", "Metatarse", "Pelvis", "Scapule", "Rib", "Vertebre", "Mandible", "Falange", "Unknow")->>Anatomic_Part}
+    switch(menu(c("Unknow", "Humero", "Femur", "Radious", "Tibia", "Ulna", "Metacarp", "Metatarse", "Pelvis", "Scapule", "Rib", "Vertebre", "Mandible", "Falange")),"Unknow", "Humero", "Femur", "Radious", "Tibia", "Ulna", "Metacarp", "Metatarse", "Pelvis", "Scapule", "Rib", "Vertebre", "Mandible", "Falange")->>Anatomic_Part}
