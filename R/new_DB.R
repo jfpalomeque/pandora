7677d74e25a49ed3c7fb545ec8dd559d837786be
jfpalomeque <jfpalomeque.gonzalez@gmail.com>
2016-07-02 22:25:41 +0100

    Compatible with UNIX systems. Delete option "load original data" from the main menu

--- R/new_DB.R
+++ R/new_DB.R
@@ -6,7 +6,7 @@ function(){#This function run all other functions in order to create a new Data 
     enter.dir()
     marks<<-data.frame()
     dir.process()
-    names<-c("site", "n_site", "n", "material", "section", "taxon_bone", "type_bone", "LM1x", "LM1y", "LM2x", "LM2y", "LM3x", "LM3y", "LM4x", "LM4y", "LM5x", "LM5y", "LM6x", "LM6y", "LM7x", "LM7y", "WIS", "WIM", "WIB", "LDC", "RDC", "SI", "d", "OA")
+    names<-c("site", "n_site", "n", "material", "section", "taxon_bone", "type_bone", "Anatom_part", "LM1x", "LM1y", "LM2x", "LM2y", "LM3x", "LM3y", "LM4x", "LM4y", "LM5x", "LM5y", "LM6x", "LM6y", "LM7x", "LM7y", "WIS", "WIM", "WIB", "LDC", "RDC", "SI", "d", "OA")
     marks<-rbind(marks,newrow)
     names(marks)<-names
     marks<<-marks
--- R/select.marks_file_add.R
+++ R/select.marks_file_add.R
@@ -3,6 +3,8 @@ function(){
   file.choose()->marks_file.path
   read.csv(marks_file.path)->>marks_add
   marks_add<-marks_add[,-1]
+  names<-c("site", "n_site", "n", "material", "section", "taxon_bone", "type_bone", "Anatom_part", "LM1x", "LM1y", "LM2x", "LM2y", "LM3x", "LM3y", "LM4x", "LM4y", "LM5x", "LM5y", "LM6x", "LM6y", "LM7x", "LM7y", "WIS", "WIM", "WIB", "LDC", "RDC", "SI", "d", "OA")
+  names(marks_add)<-names
   marks<<-rbind(marks,marks_add)
   stop(data_processing_menu())
 }
